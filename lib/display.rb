68b66c64899252bb02597465532a8fa7e9b2f81b
Cesar Jolibois <diazcd85@gmail.com>
2018-09-04 09:31:24 -0600

    added print method

--- lib/display.rb
+++ lib/display.rb
@@ -4,5 +4,14 @@ class Display
   def initialize(board)
     @board = board
   end
-  
+
+  def print
+    letters = ["A","B","C", "D"]
+    p "==========="
+    p ". 1 2 3 4"
+    4.times do |i|
+      puts "#{letters[i]} #{@board.columns[i].join(" ")}"
+    end
+    p "==========="
+  end
 end
