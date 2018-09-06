79d1f2cbd6de493ed8aa5f200b0bd566b009d3fd
Cesar Jolibois <diazcd85@gmail.com>
2018-09-05 19:09:09 -0600

    updated ship, board, display

--- test/display_test.rb
+++ test/display_test.rb
@@ -88,4 +88,25 @@ class DisplayTest < Minitest::Test
     assert_equal true, ship_2.hit?
     assert_equal "H", screen.board[1][0]
   end
+
+  def test_if_it_can_sink_a_ship
+    ship_1 = Ship.new(3)
+    ship_2 = Ship.new(2)
+    space = Board.new
+    screen = Display.new(space)
+
+
+    space.place(ship_1, "A2 A3 A4")
+    space.place(ship_2, "B1 C1")
+
+    screen.set(4)
+    screen.shoot("B1")
+    screen.shoot("C1")
+
+    assert_equal "H", screen.board[1][0]
+    assert_equal true, ship_2.hit?
+    assert_equal "H", screen.board[2][0]
+    assert_equal true, ship_2.sink?
+  end
+
 end
